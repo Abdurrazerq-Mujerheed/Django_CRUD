a6eac65088d64ac46e6ad31627e948fbe41e6215
Abdurrazaq Jibril Baba <mujerheed20@gmail.com>
2022-06-24 15:15:01 +0100

    edit view, urls file and add template folder

--- I4G000632HB0/blog/views.py
+++ I4G000632HB0/blog/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+from .models import Post
+from django.urls import reverse_lazy
 
-# Create your views here.
+class PostCreateView(CreateView):
+    model = Post
+    field = "__all__"
+    template_name = "post_form.html"
+    success_url = reverse_lazy("blog:all")
+
+class PostListView(ListView):
+    model = Post
+    template_name = "post_list.html"
+
+class PostDetailView(DetailView):
+    model = Post
+    field = "__all__"
+    template_name = "post_detail.html"
+    success_url = reverse_lazy("blog:all")
+
+class PostUpdateView(UpdateView):
+    model = Post
+    field = "__all__"
+    template_name = ""
+    success_url = reverse_lazy("blog:all")
+
+class PostDeleteView(DeleteView):
+    model = Post
+    field = "__all__"
+    template_name = "post_confirm_delete.html"
+    success_url = reverse_lazy("blog:all")
